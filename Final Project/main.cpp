90310858daf03c3d39f521a386d85a786c0c85f3
Kaela Hasan <kaelahasan@gmail.com>
2020-05-05 12:22:36 -0400

    First commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Kaela Yasmin Hasan on 5/5/20.
+//  Copyright © 2020 Kaela Yasmin Hasan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
